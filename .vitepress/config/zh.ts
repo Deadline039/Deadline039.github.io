a2625e8730ad7fbfb473e6445630f484b9402d40
Deadline039 <1315374252@qq.com>
2025-08-26 13:10:22 +0800

    update title

--- .vitepress/config/zh.ts
+++ .vitepress/config/zh.ts
@@ -134,7 +134,7 @@ function sidebarEmbedded(): DefaultTheme.SidebarItem[] {
     return [
         { text: '这里有什么？', link: 'index.md' },
         { text: 'CMSIS配置向导标注', link: 'cmsis_config_wizard.md' },
-        { text: '分散加载文件', link: 'scatter.md' },
+        { text: 'armlink与分散加载文件', link: 'scatter.md' },
     ]
 }
 
